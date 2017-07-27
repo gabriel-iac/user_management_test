3dc52349efd0d2f0db88f81dd4ac85dec2404b67
gabriele iacopetti <gabriele.iacopetti@thirdspacelearning.com>
2017-07-27 18:18:41 +0100

    changed files

--- app/controllers/htmlController.js
+++
@@ -1,7 +0,0 @@
-import bodyParser from 'body-parser';
-
-module.export = function(app) {
-app.get('/', function(req, res) {
-    res.render('index');
- });
-}
--- app/index.js
+++ app/index.js
@@ -3,24 +3,38 @@ import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
 import bodyParser from 'body-parser';
-import config from './config/config.json';
-import pg from 'pg';
+import initializeDb from './db';
+import middleware from './middleware';
+import api from './api';
+import config from './config.json';
 
 let app = express();
-
 app.server = http.createServer(app);
+
 // logger
 app.use(morgan('dev'));
-app.use(express.static(__dirname + '/views'));
+
 // 3rd party middleware
 app.use(cors({
-//add headers
+	exposedHeaders: config.corsHeaders
 }));
 
 app.use(bodyParser.json({
-  limit : config.bodyLimit
+	limit : config.bodyLimit
 }));
 
-app.server.listen(process.env.PORT || config.port, () => {
-    console.log(`Started on port ${app.server.address().port}`);
+// connect to db
+initializeDb( db => {
+
+	// internal middleware
+	app.use(middleware({ config, db }));
+
+	// api router
+	app.use('/api', api({ config, db }));
+
+	app.server.listen(process.env.PORT || config.port, () => {
+		console.log(`Started on port ${app.server.address().port}`);
+	});
 });
+
+export default app;
